0056a06f06d9683b3eebc432e115251f7d90139a
Pietro <pietroroversi@libero.it>
2020-06-25 14:03:17 -0700

    termina il gioco TicTacToe

--- src/index.js
+++ src/index.js
@@ -5,56 +5,36 @@ import "./index.css";
 /* si tratta di un componente controllato (da Board): 
  * Square non mantiene più il proprio stato, riceve valori dal componente padre Board ed a sua volta lo informa di quando viene cliccato
  */
-class Square extends React.Component {
-  //non c'è il costruttore perché square non tiene traccia dello stato della partita internamente. Ci pensa la board
+function Square(props) {
+  //Square è un componente funzione, ha solo il metodo render e non tiene traccia del proprio stato interno
+  return (
+    <button
+      className="square"
 
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+      style={{ color: props.color }}
+      onClick={props.onClick}
+
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    //questo array di stato rappresenta i 9 quadrati della board
-    this.state = {
-      squares: Array(9).fill(null),
-    }
-  }
-
-  handleClick(i) {
-    /* viene fatta una copia per il principio dell'immutabilità. Vantaggi:
-     * - Eliminare la mutazione diretta dei dati ci permette di mantenere versioni precedenti della storia della partita intatte cosicché siano riutilizzabili in seguito
-     * - 
-     */
-    const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
-  }
-
   renderSquare(i) {
     //ogni quadrato riceve il proprio valore corrente(state) dall'array degli stati della board
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)} //passo una funzione da Board a Square che viene chiamata ad ogni click. In questo modo modifico lo stato 
+        value={this.props.squares[i]}
+        color={this.props.squares[i] == 'X' ? 'green' : 'red'}
+        onClick={() => this.props.onClick(i)} //passo una funzione da Board a Square che viene chiamata ad ogni click. In questo modo modifico lo stato 
       />
     );
   }
 
   render() {
-    const status = "Next player: X";
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -76,15 +56,78 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+      stepNumber: 0,
+    };
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
+  handleClick(i) {
+    /* viene fatta una copia per il principio dell'immutabilità. Vantaggi:
+     * - Eliminare la mutazione diretta dei dati ci permette di mantenere versioni precedenti della storia della partita intatte cosicché siano riutilizzabili in seguito
+     * - rilevare i cambiamenti in oggetti immutabili è molto più semplice: se il riferimento all'oggetto è diverso dal precedente allora l'oggetto è cambiato
+     * - permette di creare componenti puri. I cambiamenti vengono identificati in modo semplice e di conseguenza si può capire quando un componente richiede una nuova renderizzazione
+     * - https://it.reactjs.org/docs/optimizing-performance.html#examples
+     */
+    const history = this.state.history.slice(0, this.state.stepNumber + 1); //in questo modo se andiamo indietro nel tempo buttiamo via la "storia futura"
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    //se il quadrato è già pieno o qualcuno ha già vinto la partita la funzione esce
+    if (calculateWinner(squares) || squares[i]) { return; }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    //step dovrebbe essere l'elemento dell'array che vado a mappare, move l'iteratore
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Go to move #' + move : 'Go to game start';
+      return (<li key={move}>
+        <button onClick={() => this.jumpTo(move)}>
+          {desc}
+        </button>
+      </li>);
+    });
+
+    let status;
+    if (winner)
+      status = 'Winner: ' + winner;
+    else
+      status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -94,3 +137,25 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
+
+//herlper function
+function calculateWinner(squares) {
+  //sono le possibili combinazioni per la vittoria
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
